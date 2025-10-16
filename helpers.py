252a867216ed714c68cab4be5c7b8f92249b5561
Hagen Telg <hagen.telg@gmail.com>
2025-10-16 15:54:39 -0600

    aod can do 1625 now.

--- helpers.py
+++ helpers.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import xarray as xr
+import atmPy.radiation.retrievals.spectral_irradiance as atmspec
+import atmPy.aerosols.physics.column_optical_properties as atmcop
+
 
 def read_SE_SMHI_PFR(p2f, no_wl = 4):
     df = pd.read_csv(p2f, skiprows=1, header=None, delimiter=r'\s+')
@@ -83,3 +86,57 @@ def read_JP_JMA_POM(p2f, no_wl = 10):
     # ds['ozone'] = df.ozone
     # ds['no2'] = df.no2
     return ds
+
+def get_langleys(ds, fnmet, lt, test = False):
+    """
+    ds: dataset
+    fnmet: path to metdata
+    lt: prleliminary langleys for initail calibration for cloud screening
+    """
+    ds = ds.copy()
+    ds.attrs['site_longitude'] += 360 
+    
+    # calibrate with preliminary calibration (the last ones)
+    gdd = atmspec.CombinedGlobalDiffuseDirect(ds.copy())
+    gdd.dataset['channel_wavelength'] = gdd.dataset.channel_wavelength.astype(float) #error in processing upstream, fix it in future versions!
+    
+    gddc = gdd.apply_calibration_langley(lt)
+    
+    dnic = gddc.direct_normal_irradiation
+    dnic.raw_data = dnic.raw_data.where(dnic.raw_data.channel < 1000, drop = True)
+    dnic.met_data = fnmet
+    dnic.ozone_data = 300
+    # dnic.aod.plot.line(x = 'datetime')
+    # dnic.aod.plot.line(x = 'datetime')
+    # plt.ylim(0,0.05)
+    
+    # get cloudmask
+    aodi = atmcop.AOD_AOT(dnic.aod)
+    cloudmask = aodi.cloudmask.cloudmask_michalsky.drop_vars('channel')
+    
+    #### Make the langleys
+    si = atmspec.CombinedGlobalDiffuseDirect(ds)
+    sir = si.direct_normal_irradiation
+    sir.settings_langley_airmass_limits = (2.5, 5)
+    # apply cloudmask
+    sir.raw_data = sir.raw_data.where(cloudmask == 0)
+
+    clean = False
+    if test:
+        out = dict(sir = sir, cloudmask = cloudmask, aodi = aodi, dnic = dnic)
+        return out
+    if clean:
+        lang = sir.langley_am
+        
+        # lang.plot(wavelength=None, show_pre_clean=False)
+        # clean the langley
+        lc = lang.clean(threshold=2)
+        langc_am = lc['langley']
+        
+        lang = sir.langley_pm
+        lc = lang.clean(threshold=2)
+        langc_pm = lc['langley']
+        
+        return langc_am, langc_pm
+    else:
+        return sir.langley_am, sir.langley_pm
